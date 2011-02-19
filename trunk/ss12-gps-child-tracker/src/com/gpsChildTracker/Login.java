8dce8e8c65f529c74e5900ad201341eb9add91d4
cmcclees217@gmail.com <cmcclees217@gmail.com@30ca604b-0ed3-5d03-3496-772372845d8d>
2011-02-19 23:01:01 +0000

    login info

---
+++ trunk/ss12-gps-child-tracker/src/com/gpsChildTracker/Login.java
@@ -0,0 +1,13 @@
+package com.gpsChildTracker;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Login extends Activity{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+	}
+}
+
