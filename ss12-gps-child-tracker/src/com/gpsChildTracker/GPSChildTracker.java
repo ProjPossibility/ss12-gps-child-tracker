a248cd239bc80dac0d2b1d49d564749206fccdb5
keithderuiter@yahoo.com <keithderuiter@yahoo.com@30ca604b-0ed3-5d03-3496-772372845d8d>
2011-02-19 23:39:24 +0000

    Has a GeoPoint set, animates to zoom level 6 on that point.  It is in Nigeria.

--- ss12-gps-child-tracker/src/com/gpsChildTracker/GPSChildTracker.java
+++ ss12-gps-child-tracker/src/com/gpsChildTracker/GPSChildTracker.java
@@ -3,6 +3,7 @@ package com.gpsChildTracker;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
@@ -20,6 +21,12 @@ public class GPSChildTracker extends MapActivity {
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         //((TextView) findViewById(R.id.coolid)).setText("cool");
+        
+        GeoPoint GP = new GeoPoint(12000000, 3000000);
+        MapView MV = (MapView) findViewById(R.id.mapview);
+        
+        MV.getController().animateTo(GP);
+        MV.getController().setZoom(6);
     }
     
     @Override
