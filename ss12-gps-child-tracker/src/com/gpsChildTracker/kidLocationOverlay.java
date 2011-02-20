29a6c430adf2cb359ab3878c8c8a76fae834ce21
keithderuiter@yahoo.com <keithderuiter@yahoo.com@30ca604b-0ed3-5d03-3496-772372845d8d>
2011-02-20 03:34:13 +0000

    Includes my kid class

--- ss12-gps-child-tracker/src/com/gpsChildTracker/kidLocationOverlay.java
+++ ss12-gps-child-tracker/src/com/gpsChildTracker/kidLocationOverlay.java
@@ -1,30 +1,39 @@
 package com.gpsChildTracker;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
 public class kidLocationOverlay extends Overlay {
 	
-	public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
-		/*
+	GeoPoint point;
+	
+	public kidLocationOverlay(GeoPoint p){
+		point = p;
+	}
+	
+	public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+		
 		super.draw(canvas, mapView, shadow);
 		Paint paint = new Paint();
 		// Converts lat/lng-Point to OUR coordinates on the screen.
 		Point myScreenCoords = new Point();
-		mapView.getProjection().toPixels(p, myScreenCoords);
+		mapView.getProjection().toPixels(point, myScreenCoords);
 		paint.setStrokeWidth(1);
 		paint.setARGB(255, 255, 255, 255);
 		paint.setStyle(Paint.Style.STROKE);
-		Bitmap bmp = BitmapFactory.decodeResource( getResources(), R.drawable.marker);
+		/*
+		Bitmap bmp = BitmapFactory.decodeResource( getResources(), R.drawable.icon);
 		canvas.drawBitmap(bmp, myScreenCoords.x, myScreenCoords.y, paint);
 		canvas.drawText("Here I am...", myScreenCoords.x, myScreenCoords.y, paint);
 		*/
-		return true;
+		
+		canvas.drawPoint(myScreenCoords.x, myScreenCoords.y, paint);
 		}
 }
